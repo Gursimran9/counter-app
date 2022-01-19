623546e12b7c3dac824e86ec97a9ef36dd91d993
Gursimran9 <simrangur12345@gmail.com>
2022-01-18 22:06:53 -0500

    Final Commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import './Counter.css';
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className='counter'>
-      <AddCircleIcon style={{ fontSize: '60px', color: '#03A82D' }} />
-      <RemoveCircleIcon style={{ fontSize: '60px', color: '#F3280C' }} />
+    <div className={`counter ${counter >= 0 ? 'positive' : 'negative'}`}>
+      <AddCircleIcon
+        style={{ fontSize: '60px', color: '#03A82D' }}
+        onClick={() => setCounter(counter + 1)}
+      />
+      <h1>{counter}</h1>
+      <RemoveCircleIcon
+        style={{ fontSize: '60px', color: '#F3280C' }}
+        onClick={() => setCounter(counter - 1)}
+      />
     </div>
   );
 };
